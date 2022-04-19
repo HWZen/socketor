f502d67b55a9decb5516af80901f7711749801de
HWZen <1041453977@qq.com>
2022-04-20 00:07:32 +0800

    update test sample

--- Example/test/client_test.cpp
+++ Example/test/client_test.cpp
@@ -1,71 +1,21 @@
 #include "client.h"
-#include <string>
-
-#if __has_include("fast_io.h") && defined(__cpp_concepts)
-#include <fast_io.h>
-#else
-
 #include <iostream>
+#include <string>
+using std::cin, std::cout, std::endl, std::string;
 
-#ifdef __cpp_concepts
-template <typename T>
-concept can_print = requires(std::ostream os, T ty)
-{
-    os << ty;
-};
-#else
-
-#define can_print typename
-#endif //__cpp_concepts
-
-template<can_print ...Args>
-decltype(auto) print(Args&& ...args)
-{
-    return (std::cout << ... << args);
-}
-
-template<can_print ...Args>
-decltype(auto) println(Args&& ...args)
-{
-    return (std::cout << ... << args) << "\n";
-}
-
-template<can_print ...Args>
-decltype(auto) perr(Args&& ...args)
-{
-    return (std::cerr << ... << args) << "\n";
-}
-
-#endif //__has_include("fast_io.h") && defined(__cpp_concepts)
-
-int main(int argc, char **argv) try
-{
-    std::string hostName = argc > 1 ? argv[1] : "127.0.0.1";
-
-    mysock::Client client(hostName.c_str(), 5150);
-
-    int check = client.connect2server();
-
-    if (check != mysock::SUCESS)
-        throw (mysock::exception<std::string>(std::string("connect fail: ") + std::to_string(check)));
-
-    for (int i{}; i < 1000000; ++i)
+int main(){
+    mysock::Client c;
+    if(auto err = c.Connect2Server(); err != mysock::SUCESS)
     {
-        client.rawSend((const void*)&i, sizeof(int));
+        cout << "connect fail.\n";
+        return 1;
     }
-    int i = -1;
-    client.rawSend(&i, sizeof(int));
-    char buf[2];
-    client.receive(buf, 2);
 
+    string s;
+    cin >> s;
+
+    c.Send(s);
+    c.CloseConnect();
 
     return 0;
-}
-catch (mysock::exception<int>& e)
-{
-    perr(e.date(), "\n", "error code: ", e.describe());
-}
-catch (mysock::exception<std::string>& e)
-{
-    perr(e.what());
 }
--- Example/test/server_test.cpp
+++ Example/test/server_test.cpp
@@ -1,107 +1,18 @@
 #include "server.h"
-#include <string>
-#include "function.h"
-#if __has_include("fast_io.h") && defined(__cpp_concepts)
-#include <fast_io.h>
-#else
-
 #include <iostream>
-#ifdef __cpp_concepts
-template <typename T>
-concept can_print = requires(std::ostream os, T ty)
-{
-    os << ty;
-};
-#else
-#define can_print typename
-#endif// __cpp_concepts
+#include <string>
 
-template<can_print ...Args>
-decltype(auto) print(Args &&...args)
-{
-    return (std::cout << ... << args);
-}
+using std::cin, std::cout, std::endl, std::string;
 
-template<can_print ...Args>
-decltype(auto) println(Args &&...args)
-{
-    return (std::cout << ... << args) << "\n";
-}
+int main(){
+    mysock::Server server;
+    server.Listen();
+    server.Accept([](mysock::socketor sock){
+        cout << "accepted: " << sock.address() << endl;
+        string res = sock.receive();
+        cout << "received: " << res << endl;
+        mysock::Server::CloseConnect(sock);
+    });
 
-template<can_print ...Args>
-decltype(auto) perr(Args &&...args)
-{
-    return (std::cerr << ... << args) << "\n";
-}
-
-#endif //__has_include("fast_io.h") && defined(__cpp_concepts)
-
-using namespace std;
-
-void foo(mysock::socketor client);
-
-int main()
-{
-    mysock::Server server(5150);
-    try
-    {
-        if(int err = server.Listen(); err != mysock::SUCESS)
-            throw mysock::exception<mysock::flag>(mysock::flag(err), "Listen fail");
-        println("Listening");
-        server.Accept(foo);
-        println("waiting for accept");
-    }
-    catch (mysock::exception<mysock::flag> &e)
-    {
-        switch (e.date())
-        {
-        case mysock::flag::BIND_FAIL:
-            perr(EnumName<mysock::flag,mysock::flag::BIND_FAIL>());
-            break;
-        case mysock::flag::LISTEN_FAIL:
-            perr(EnumName<mysock::flag,mysock::flag::LISTEN_FAIL>());
-            break;
-        case mysock::LISTEN_SUCESS:
-            break;
-        default:
-            perr("unknow error: ", e.date());
-            perr("error description: ", e.what());
-            break;
-        }
-    }
-    catch (std::exception &e)
-    {
-        perr(e.what());
-    }
-
-}
-
-void foo(mysock::socketor client)
-{
-    println("get accept: ", client.address());
-    int cnt = 0;
-    vector<int> error_buf;
-
-    int *buf = new int{};
-    do{
-        if(int i = client.receive(buf,4);i <= SOCKET_ERROR){
-            perr("recv err: ", i);
-            break;
-        }
-        if(*buf == -1)
-            break;
-        if(cnt != *buf)
-        {
-            error_buf.push_back(*buf);
-            println(*buf);
-            cnt = *buf;
-        }
-        ++cnt;
-
-
-    }
-    while(*buf != -1);
-
-    println("lose size: ", error_buf.size());
-
+    server.CloseServer();
 }
